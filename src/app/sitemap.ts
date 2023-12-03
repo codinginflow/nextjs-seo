fc9b8b70b0437c8712b33a54707f56edc17a426b
Coding in Flow <info@codinginflow.com>
2023-12-03 12:53:59 +0100

    End of part 6

---
+++ src/app/sitemap.ts
@@ -0,0 +1,22 @@
+import { BlogPostsResponse } from "@/models/BlogPost";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const response = await fetch("https://dummyjson.com/posts");
+  const { posts }: BlogPostsResponse = await response.json();
+
+  const postEntries: MetadataRoute.Sitemap = posts.map(({ id }) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/posts/${id}`,
+    // lastModified: new Date(post.updatedAt),
+    // changeFrequency:,
+    // priority:
+  }));
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+      lastModified: new Date(),
+    },
+    ...postEntries,
+  ];
+}
