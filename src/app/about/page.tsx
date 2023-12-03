fbdfd7354dde5d7a6e6ee812e5df8ef05d045656
Coding in Flow <info@codinginflow.com>
2023-12-03 10:53:54 +0100

    End of part 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+};
+
 export default function AboutPage() {
   return (
     <div className="max-w-prose m-auto space-y-3">
--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Privacy Policy",
+};
+
 export default function PrivacyPolicy() {
   return (
     <div className="max-w-prose m-auto space-y-3">
